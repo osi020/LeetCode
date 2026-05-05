6278957da0370ffb9bf21de267631f9660fb6fd8
osi020 <oussamabeddad8@gmail.com>
2026-05-05 15:04:13 +0100

    The cpp solution for Leetcode problem 21 about removing elements using the two pointers method

---
+++ Remove_Elements_21/Remove_Elements.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector> // This librairie
+
+class Solution {
+public: 
+    int removeElement(std::vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::vector<int> nums = {3, 2, 2, 3}; 
+    int result = solution.removeElement(nums, 3);
+    std::cout << "Nouveau k : " << result << std::endl; // Correction de std::cout
+    return 0;
+}
